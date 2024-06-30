e52bcc9865415c6c08b214465cebaca56cdfd93e
kassemKu <arabisc95@gmail.com>
2024-06-30 14:15:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#canvas");
+// console.log(innerHeight);
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+// console.log(innerWidth);
+canvas.height = innerHeight;
+canvas.width = innerWidth;
