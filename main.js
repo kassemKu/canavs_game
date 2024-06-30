568ccfa033d1aabb9e2237bf41a4745d96965dc4
kassemKu <arabisc95@gmail.com>
2024-06-30 14:56:09 +0300

    update: playrt class

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+// 1- select canvas element
 const canvas = document.querySelector("#canvas");
+// 2- check selector with console
+// console.log(canvas);
+// 3- explain innerWidth / innerHeight
 // console.log(innerHeight);
 // console.log(window.innerHeight);
 // console.log(window.innerWidth);
 // console.log(innerWidth);
+// 4- gave the canvas full screen width and height
 canvas.height = innerHeight;
 canvas.width = innerWidth;
+// 5- fix the default screen margin, remove body margin
+// 6- create canvas context api, read more about canvas context api
+const ctx = canvas.getContext("2d");
+// context can be 2d or 3d we need 2d in our project
+console.log(ctx);
+// 7- create a class to draw a circle
+const Player = {
+  // ask about player property
+  // size
+  // color
+  // position
+  // velocity
+  // 8- create class constructor / explain what it is
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  },
+};
